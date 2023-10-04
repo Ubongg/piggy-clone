6ad53d2216ae6c352cb5af80fc7d5f837c9841ea
Ubongg <ubongeffiong5@gmail.com>
2023-10-04 12:36:36 +0100

    styled make safelock component

--- src/components/makeSafelock/MakeSafelock.jsx
+++ src/components/makeSafelock/MakeSafelock.jsx
@@ -1,12 +1,21 @@
-import { Box, Typography, Drawer } from "@mui/material";
+import { Box, Typography, Drawer, Button } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import CloseIcon from "@mui/icons-material/Close";
 import { useGlobalContext } from "../context/context";
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "@/app/globals.css";
 
 const MakeSafelock = ({ toggleCreateSafelockDrawer, anchor }) => {
   const theme = useTheme();
   const { safeColor, greyBorder } = useGlobalContext();
+  const falseDate = (date) => new Date() < date;
+  const [payBackDate, setPayBackDate] = useState(new Date());
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <Box
@@ -21,7 +30,6 @@ const MakeSafelock = ({ toggleCreateSafelockDrawer, anchor }) => {
         },
       }}
       role="presentation"
-      onKeyDown={toggleCreateSafelockDrawer(anchor, false)}
     >
       <Box
         sx={{
@@ -49,17 +57,98 @@ const MakeSafelock = ({ toggleCreateSafelockDrawer, anchor }) => {
             fontWeight: 600,
           }}
         >
-          How long do you want to lock funds?
+          Create a Safelock to Lock Funds
         </Typography>
-        <Typography
-          variant="p"
-          sx={{
-            fontSize: "0.9rem",
+        <form
+          style={{
+            paddingTop: "30px",
+            display: "flex",
+            flexDirection: "column",
           }}
+          onSubmit={handleSubmit}
         >
-          Select a duration that you want to lock your funds & earn upfront
-          interests of up to 35.6%
-        </Typography>
+          <label
+            style={{
+              margin: "5px 0",
+              fontSize: "0.75rem",
+              fontWeight: 600,
+            }}
+          >
+            Amount to Lock / Invest
+          </label>
+          <input
+            type="text"
+            placeholder="25000"
+            required
+            style={{
+              outline: "none",
+              padding: "17px 15px 19px",
+              border: "none",
+              background: "#edf2f7",
+              marginBottom: "30px",
+              width: "100%",
+              fontSize: "1rem",
+              borderRadius: "0.5rem",
+            }}
+          />
+          <label
+            style={{
+              margin: "5px 0",
+              fontSize: "0.75rem",
+              fontWeight: 600,
+            }}
+          >
+            Title of SafeLock™
+          </label>
+          <input
+            type="text"
+            placeholder="My New Lock"
+            required
+            style={{
+              outline: "none",
+              padding: "17px 15px 19px",
+              border: "none",
+              background: "#edf2f7",
+              marginBottom: "30px",
+              width: "100%",
+              fontSize: "1rem",
+              borderRadius: "0.5rem",
+            }}
+          />
+          <label
+            style={{
+              margin: "5px 0",
+              fontSize: "0.75rem",
+              fontWeight: 600,
+            }}
+          >
+            Set Payback Date
+          </label>
+          <DatePicker
+            filterDate={falseDate}
+            minTime={new Date(0, 0, 0, 9, 0)}
+            maxTime={new Date(0, 0, 0, 19, 0)}
+            selected={payBackDate}
+            onChange={(payBackDate) => setPayBackDate(payBackDate)}
+            required
+            className="datePickerStyle"
+          />
+          <Button
+            variant="contained"
+            sx={{
+              py: "12px",
+              fontWeight: 600,
+              fontSize: "1rem",
+              mt: "20px",
+              borderBottomRightRadius: "0.5rem",
+              borderTopLeftRadius: "0.5rem",
+              borderTopRightRadius: "0.5rem",
+              borderBottomLeftRadius: 0,
+            }}
+          >
+            submit
+          </Button>
+        </form>
       </Box>
     </Box>
   );
